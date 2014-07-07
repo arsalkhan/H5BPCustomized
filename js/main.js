3a6c214f607528fcefcea3a5fe39de7176237440
Arsal Khan <khanarsal@live.com>
2014-07-07 11:15:50 -0700

    Files Added
    New Files Added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery.noConflict();
+
+(function($) {
+    //use '$' as you normally would
+    $(document).ready(function() {
+        //code here that depends on the document being fully loaded
+    });
+
+})(jQuery);  
+
